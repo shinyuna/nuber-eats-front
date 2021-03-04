eef6d9c63214a57be4dd1d6e366841c1de287a94
신유나 <yunadev01@gmail.com>
2021-03-05 02:59:29 +0900

    Update router & apollo client - login router에서 유저 정보를 불러오기 위한 쿼리 작성, request header에 토큰 저장

--- src/apollo.ts
+++ src/apollo.ts
@@ -1,12 +1,25 @@
-import { ApolloClient, InMemoryCache, makeVar } from '@apollo/client';
+import { ApolloClient, createHttpLink, InMemoryCache, makeVar } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import { AUTH_TOKEN } from './constants';
 
 const token = localStorage.getItem(AUTH_TOKEN);
 export const isLoggedInVar = makeVar(Boolean(token));
 export const authToken = makeVar(token);
 
-export const client = new ApolloClient({
+const httpLink = createHttpLink({
   uri: 'http://localhost:5000/graphql',
+});
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      'x-jwt': authToken() || '',
+    },
+  };
+});
+
+export const client = new ApolloClient({
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache({
     typePolicies: {
       Query: {
--- src/routers/LoggedInRouter.tsx
+++ src/routers/LoggedInRouter.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import { gql, useQuery } from '@apollo/client';
+import { meQuery } from '../api-types/meQuery';
+
+const ME_QUERY = gql`
+  query meQuery {
+    me {
+      id
+      email
+      role
+      verify
+    }
+  }
+`;
 
 export const LoggedInRouter = () => {
+  const { data, loading, error } = useQuery<meQuery>(ME_QUERY);
+  if (!data || loading || error) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <span className="text-xl font-medium tracking-wide">Loading...</span>
+      </div>
+    );
+  }
   return (
     <div>
-      <h1>Logged In</h1>
+      <h1>{data.me.role}</h1>
     </div>
   );
 };
