b63c90407b09db81e91b84398c800cf9ab7c2a4d
신유나 <yunadev01@gmail.com>
2021-03-05 00:35:31 +0900

    Extend tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,28 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    screens: {
+      sm: { min: '320px', max: '767px' },
+      md: { min: '768px', max: '1023px' },
+      lg: { max: '1279px' },
+      xl: { max: '1535px' },
+    },
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        trueGray: colors.trueGray,
+        lime: colors.lime,
+        lightblue: colors.lightBlue,
+        uber: {
+          light: '#DFF0D8',
+          DEFAULT: '#5fb70a',
+          dark: '#3e981f',
+        },
+      },
+    },
   },
   variants: {
     extend: {},
