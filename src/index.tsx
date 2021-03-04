ef6df0135a180ceb5aa7b3aa35ebc4db1fb8e605
신유나 <yunadev01@gmail.com>
2021-03-05 00:37:00 +0900

    Setting apllo client

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { ApolloProvider } from '@apollo/client';
+import { client } from './apollo';
 import './assets/styles/style.css';
 import reportWebVitals from './reportWebVitals';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
