9a411caff6e5e02b5dea29861ab03c08843e8c83
신유나 <yunadev01@gmail.com>
2021-04-05 22:38:15 +0900

    Update Router

--- src/components/Restaurant.tsx
+++ src/components/Restaurant.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 interface IRestaurantProps {
   id: number;
@@ -7,21 +8,23 @@ interface IRestaurantProps {
   categoryName?: string;
 }
 
-export const Restaurant: React.FC<IRestaurantProps> = ({ name, coverImage, categoryName }) => {
+export const Restaurant: React.FC<IRestaurantProps> = ({ id, name, coverImage, categoryName }) => {
   return (
-    <div className="cursor-pointer ">
-      <div
-        className="py-20 mb-2 bg-gray-200 bg-center bg-cover"
-        style={{ backgroundImage: `url(${coverImage})` }}
-      ></div>
-      {/* <div className="flex items-center justify-between">
+    <Link to={`/restaurant/${id}`}>
+      <div className="cursor-pointer ">
+        <div
+          className="py-20 mb-2 bg-gray-200 bg-center bg-cover"
+          style={{ backgroundImage: `url(${coverImage})` }}
+        ></div>
+        {/* <div className="flex items-center justify-between">
         <h3 className="text-lg font-medium">{name}</h3>
         <p className="px-2 py-1 text-sm text-green-500 rounded-full bg-green-50">{categoryName}</p>
       </div> */}
-      <div>
-        <h3 className="mt-1 text-lg font-medium border-b">{name}</h3>
-        <p className="mt-2 text-xs text-gray-400 capitalize">{categoryName}</p>
+        <div>
+          <h3 className="mt-1 text-lg font-medium border-b">{name}</h3>
+          <p className="mt-2 text-xs text-gray-400 capitalize">{categoryName}</p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
--- src/routers/LoggedInRouter.tsx
+++ src/routers/LoggedInRouter.tsx
@@ -7,22 +7,22 @@ import { ConfirmEmail } from '../pages/user/ConfirmEmail';
 import { EditProfile } from '../pages/user/EditProfile';
 import { NotFound } from '../pages/404';
 import { Search } from '../pages/client/Search';
+import { RestaurantDetail } from '../pages/client/RestaurantDetail';
+import { MyRestaurant } from '../pages/owner/MyRestaurant';
+import { AddRestaurant } from '../pages/owner/AddRestaurant';
 
-const ClientRoutes = [
-  <Route path="/" key="feed" exact>
-    <Feed />
-  </Route>,
-  <Route path="/search" key="search" exact>
-    <Search />
-  </Route>,
-  <Route path="/edit-profile" key="edit-profile">
-    <EditProfile />
-  </Route>,
+const commonRoutes = [
+  { path: '/edit-profile', component: <EditProfile /> },
+  { path: '/confirm', component: <ConfirmEmail /> },
 ];
-const OwnerRoutes = [
-  <Route path="/" key="feed" exact>
-    <Feed />
-  </Route>,
+const clientRoutes = [
+  { path: '/', component: <Feed /> },
+  { path: '/search', component: <Search /> },
+  { path: '/restaurant/:id', component: <RestaurantDetail /> },
+];
+const ownerRoutes = [
+  { path: '/', component: <MyRestaurant /> },
+  { path: '/add-restaurant', component: <AddRestaurant /> },
 ];
 
 export const LoggedInRouter = () => {
@@ -38,11 +38,23 @@ export const LoggedInRouter = () => {
     <Router>
       <Header />
       <Switch>
-        {data.me.role === 'Owner' && OwnerRoutes}
-        {data.me.role === 'Client' && ClientRoutes}
-        <Route path="/confirm">
-          <ConfirmEmail />
-        </Route>
+        {data.me.role === 'Owner' &&
+          ownerRoutes.map(route => (
+            <Route path={route.path} key={route.path} exact>
+              {route.component}
+            </Route>
+          ))}
+        {data.me.role === 'Client' &&
+          clientRoutes.map(route => (
+            <Route path={route.path} key={route.path} exact>
+              {route.component}
+            </Route>
+          ))}
+        {commonRoutes.map(route => (
+          <Route path={route.path} key={route.path} exact>
+            {route.component}
+          </Route>
+        ))}
         <Route>
           <NotFound />
         </Route>
