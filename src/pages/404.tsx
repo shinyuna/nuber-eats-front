437d72c42a32c7afb45812ca7276b2fb2234b9bf
신유나 <yunadev01@gmail.com>
2021-04-05 22:36:29 +0900

    Update 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { HelmetTitle } from '../components/HelmetTitle';
 
-export const NotFound = () => (
-  <div className="flex flex-col items-center justify-center h-screen">
+export const NotFound: React.VFC = () => (
+  <div className="max-w-md mx-auto mt-40 text-center">
     <HelmetTitle title={'Page not found | Nuber Eats'} />
-    <h2 className="mb-4 text-2xl font-semibold">Page Not Found.</h2>
-    <p className="mb-4 text-lg">The page you're looking for does not exist or has moved.</p>
-    <Link to="/" className="hover:underline text-uber">
-      Go back home &rarr;
+    <p className="text-8xl">🥝🥬</p>
+    <h4 className="mt-10 text-4xl">Nothing to eat here...</h4>
+    <p className="my-5 text-lg font-light">Let’s discover something delicious.</p>
+    <Link to="/" className="block w-full p-4 text-white bg-lime-600">
+      Find Food &rarr;
     </Link>
   </div>
 );
