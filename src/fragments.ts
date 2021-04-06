7c331aa153a53e2d6687c339e566a671c80e4942
신유나 <yunadev01@gmail.com>
2021-04-07 01:50:47 +0900

    Update Fragment

--- src/fragments.ts
+++ src/fragments.ts
@@ -12,3 +12,35 @@ export const RESTAURANT_FRAGMENT = gql`
     }
   }
 `;
+
+export const CATEGORY_FRAGMENT = gql`
+  fragment CategoryParts on Category {
+    id
+    name
+    coverImage
+    slug
+    restaurantCount
+  }
+`;
+
+export const DISH_FRAGMENT = gql`
+  fragment DishParts on Dish {
+    id
+    name
+    photo
+    price
+    description
+    options {
+      name
+      price
+      choices {
+        name
+        price
+      }
+    }
+  }
+`;
+
+// export const MENU_FRAGMENT = gql`
+//   fragment MenuParts on Menu {}
+// `;
