c75d5f7190a047a2c5d425e1f5548c185e4a5b1f
신유나 <yunadev01@gmail.com>
2021-05-26 18:25:49 +0900

    Update utils function - js doc

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,12 +1,12 @@
 /**
  * s3ImageUpload
- * @param file
- * @param userId
- * @param fileType
+ * @param file - file
+ * @param userId - owner id
+ * @param fileType - ex: restaurant_cover_img
  * @description 사용자가 업로드한 이미지를 s3에 업로드 후 해당 s3 파일 url을 반환해주는 함수
  * @returns s3 upload image url
  */
-export const s3ImageUpload = async (file: FileList, userId: string, fileType: string) => {
+export const s3ImageUpload = async (file: FileList | string, userId: string, fileType: string) => {
   const formData = new FormData();
   const actualFile = file[0];
 
