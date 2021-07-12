3965c5a8e1e1258f63156112eb2ad0d217dd36f1
신유나 <yunadev01@gmail.com>
2021-07-12 16:12:25 +0900

    Update component - Menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,3 +1,5 @@
+import { faEllipsisH } from '@fortawesome/pro-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { DishParts } from '../api-types/DishParts';
 import { numberWithCommas } from '../formatters';
 
@@ -5,11 +7,15 @@ interface IMenuProps {
   menu: DishParts;
   isCustomer?: boolean;
   makeOrder?: () => void;
+  openControler?: () => void;
 }
 
-export const Menu = ({ menu, isCustomer = false, makeOrder }: IMenuProps) => {
+export const Menu = ({ menu, isCustomer = false, makeOrder, openControler }: IMenuProps) => {
   return (
-    <div className={`flex ${isCustomer ? 'cursor-pointer' : undefined}`} onClick={isCustomer ? makeOrder : undefined}>
+    <div
+      className={`flex relative ${isCustomer ? 'cursor-pointer' : undefined}`}
+      onClick={isCustomer ? makeOrder : undefined}
+    >
       <div className="flex flex-col justify-between flex-1 p-4 border border-gray-200">
         <div className="tracking-wide">
           <p className="text-medium">{menu.name}</p>
@@ -20,6 +26,14 @@ export const Menu = ({ menu, isCustomer = false, makeOrder }: IMenuProps) => {
       <div className="flex-1 flex-shrink h-40 bg-gray-200">
         <img className="object-cover w-full h-full" src={menu.photo} alt={menu.name} />
       </div>
+      {!isCustomer && (
+        <button
+          className="absolute px-2 bg-white rounded-full shadow-md text-uber right-2 top-2"
+          onClick={openControler}
+        >
+          <FontAwesomeIcon icon={faEllipsisH} className="text-2xl align-middle" />
+        </button>
+      )}
     </div>
   );
 };
