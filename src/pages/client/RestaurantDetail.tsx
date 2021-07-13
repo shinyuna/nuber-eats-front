dceb9f971dd35f343805eb02cd8b90a33b6d974d
신유나 <yunadev01@gmail.com>
2021-07-13 13:10:47 +0900

    Update RestaurantDetail - 레스토랑 배너 컴포넌트화

--- src/pages/client/RestaurantDetail.tsx
+++ src/pages/client/RestaurantDetail.tsx
@@ -8,6 +8,7 @@ import { Menu } from '../../components/Menu';
 import { DishParts } from '../../api-types/DishParts';
 
 import OrderScreen from '../../components/OrderScreen';
+import RestaurantBanner from '../../components/RestaurantBanner';
 
 const RESTAURANT_QUERY = gql`
   query getRestaurant($input: RestaurantInput!) {
@@ -36,11 +37,6 @@ export const RestaurantDetail = () => {
   const [orderVisible, setOrderVisible] = useState<boolean>(false);
   const [peekMenu, setPeekMenu] = useState<DishParts>();
 
-  const openOrderScreen = (menu: DishParts) => {
-    setOrderVisible(true);
-    setPeekMenu(menu);
-  };
-
   const { loading, data } = useQuery<getRestaurant, getRestaurantVariables>(RESTAURANT_QUERY, {
     variables: {
       input: {
@@ -49,29 +45,21 @@ export const RestaurantDetail = () => {
     },
   });
 
+  const openOrderScreen = (menu: DishParts) => {
+    setOrderVisible(true);
+    setPeekMenu(menu);
+  };
+
+  console.log('🚀 ~ RestaurantDetail ~ data', data);
+
   return (
     <main>
-      <div
-        className="relative py-48 bg-gray-200 bg-center bg-cover "
-        style={{
-          backgroundImage: `url(${data?.getRestaurant.restaurant?.coverImage})`,
-        }}
-      >
-        <div
-          className="absolute bottom-0 left-0 flex items-end w-full h-1/2"
-          style={{ background: `linear-gradient(0deg, rgba(0, 0, 0, 0.72), rgba(0, 0, 0, 0) 100%)` }}
-        >
-          <div className="px-10 py-6 text-white">
-            <div>
-              <h4 className="page-h4">{data?.getRestaurant.restaurant?.name}</h4>
-              <h5 className="mb-1 text-sm">{data?.getRestaurant.restaurant?.category?.name}</h5>
-              <p className="text-sm">
-                {data?.getRestaurant.restaurant?.address} • <button className="font-medium underline">More info</button>
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
+      <RestaurantBanner
+        coverImage={data?.getRestaurant.restaurant?.coverImage!}
+        name={data?.getRestaurant.restaurant?.name!}
+        category={data?.getRestaurant.restaurant?.category?.name!}
+        address={data?.getRestaurant.restaurant?.address!}
+      />
       <div className="px-10 py-6 sm:px-5">
         <h4 className="mb-5 text-xl font-medium">{data?.getRestaurant.restaurant?.name} Menu</h4>
         {data?.getRestaurant.restaurant?.menu.length === 0 ? (
