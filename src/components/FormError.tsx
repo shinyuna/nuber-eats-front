d7ec5d9fc6fa3fe453b696a7c2112f02ec08f090
신유나 <yunadev01@gmail.com>
2021-07-25 19:19:59 +0900

    Update component[FormError] - add props, margin

--- src/components/FormError.tsx
+++ src/components/FormError.tsx
@@ -3,8 +3,9 @@ import React from 'react';
 interface IFormErrorProps {
   errMsg: string;
   location?: string;
+  margin?: string;
 }
 
-export const FormError: React.FC<IFormErrorProps> = ({ errMsg, location = 'left' }) => (
-  <p className={`text-${location} text-red-500`}>{errMsg}</p>
+export const FormError: React.FC<IFormErrorProps> = ({ errMsg, location = 'left', margin }) => (
+  <p className={`text-${location} text-red-500 text-sm ${margin}`}>{errMsg}</p>
 );
