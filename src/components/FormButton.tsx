83c7d9802f9dfc69835aee4c58b87e3b508f54b2
신유나 <yunadev01@gmail.com>
2021-07-29 20:38:22 +0900

    Update Component - form button

--- src/components/FormButton.tsx
+++ src/components/FormButton.tsx
@@ -2,10 +2,14 @@ interface IButtonProps {
   isLoading: boolean;
   isValid: boolean;
   actionText: string;
+  onClick?: () => void;
 }
 
-export const FormButton: React.FC<IButtonProps> = ({ isLoading, isValid, actionText }) => (
-  <button className={`button mt-4 ${isValid ? 'bg-uber hover:bg-uber-dark' : 'bg-gray-200 pointer-events-none'}`}>
+export const FormButton: React.FC<IButtonProps> = ({ isLoading, isValid, actionText, onClick }) => (
+  <button
+    className={`button mt-4 ${isValid ? 'bg-uber hover:bg-uber-dark' : 'bg-gray-200 pointer-events-none'}`}
+    onClick={onClick}
+  >
     {isLoading ? 'Loading...' : actionText}
   </button>
 );
