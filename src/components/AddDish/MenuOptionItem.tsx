f910e39118562a2fa583c94f42bbadce01b35904
신유나 <yunadev01@gmail.com>
2021-09-09 01:11:38 +0900

    [Update] MenuOptionItem UI

--- src/components/AddDish/MenuOptionItem.tsx
+++ src/components/AddDish/MenuOptionItem.tsx
@@ -7,25 +7,19 @@ interface MenuOptionItemProps {
 
 const MenuOptionItem = ({ option }: MenuOptionItemProps) => {
   return (
-    <div className="flex-1 p-4 border rounded-md border-lime-500 bg-lime-50 text-lime-600">
+    <div className="flex-1 p-4 rounded-md bg-lime-100 text-lime-600">
       <h4 className="mb-2 text-base font-semibold">{option.name}</h4>
       {option.choices.map((choice, index) => (
         <p className="text-sm" key={index}>
-          <span className="mr-2">{choice.name}</span>
-          <span>{numberWithCommas(choice.price)}₩</span>
+          <span>
+            {choice.name} ({numberWithCommas(choice.price)}원)
+          </span>
         </p>
       ))}
-      <p className="text-sm">
-        <span className="mr-2">필수 여부</span>
-        <span>{option.isRequired ? '🙆‍♀️' : '🙅‍♀️'}</span>
-      </p>
-      <p className="text-sm">
-        <span className="mr-2">최소 선택 수</span>
-        <span>{option.min}</span>
-      </p>
-      <p className="text-sm">
-        <span className="mr-2">최대 선택 수</span>
-        <span>{option.max}</span>
+      <p className="mt-2 text-sm">
+        <span className="mr-2">필수 여부 : {option.isRequired ? '🙆‍♀️' : '🙅‍♀️'}</span>
+        <span className="mr-2">최소 선택 수 : {option.min}</span>
+        <span>최대 선택 수 : {option.max}</span>
       </p>
     </div>
   );
